8a775b4f711b19f4ab5037ddcc0a62537ccd7085
Julien Samama <allinlabs@gmail.com>
2020-03-15 20:30:24 +0100

    first commit

---
+++ webdesign/smokebay/fichiers/searchbox.js
@@ -0,0 +1,10 @@
+function setAll()
+{
+	document.forms['q'].elements['a'].checked = false;
+	document.forms['q'].elements['b'].checked = false;
+	document.forms['q'].elements['c'].checked = false;
+	document.forms['q'].elements['d'].checked = false;
+	document.forms['q'].elements['r'].checked = false;
+}
+
+function rmAll() { document.forms['q'].elements['all'].checked = false; }
